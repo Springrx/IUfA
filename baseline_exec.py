0e2abdfb3959016ce41303886037c3e355e3cb3c
springrx <kangchunrx@163.com>
2024-07-19 17:02:30 +0800

    add: basic prompt

--- baseline_exec.py
+++ baseline_exec.py
@@ -1,8 +1,19 @@
 
 from prompts import sys_prompt,baseline_prompt
-from utils import ask_gpt_4o, get_real_prompt
-task_desc='给我把手电筒给开开'
-baseline_prompt=get_real_prompt(baseline_prompt,task_desc)
-print(baseline_prompt)
-response=ask_gpt_4o(sys_prompt,baseline_prompt)
-print(response)
+from utils import ask_gpt_4o, get_real_prompt, get_task_desc_from_csv,write_to_csv
+import json
+task_desc_file_path='test.csv'
+task_after_format_file_path='test_after_format.csv'
+
+task_desc=get_task_desc_from_csv(task_desc_file_path)
+task_after_format=[]
+def get_real_response(response):
+  response=json.loads(response)
+  return response['command']
+for task in task_desc:
+  real_prompt=get_real_prompt(baseline_prompt,task)
+  response=ask_gpt_4o(sys_prompt,real_prompt)
+  response=get_real_response(response)
+  print('response:',response) 
+  # task_after_format.append(response)
+# write_to_csv(task_after_format_file_path,response)
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
-client = OpenAI(api_key='you own key')
+import csv
+client = OpenAI(api_key='your key')
 def ask_gpt_4o(sys_prompt,content):
   completion = client.chat.completions.create(
     model="gpt-4o",
@@ -7,8 +8,27 @@ def ask_gpt_4o(sys_prompt,content):
       {"role": "system", "content": sys_prompt},
       {"role": "user", "content":content}
     ],
+    temperature=0,
+    response_format={ "type": "json_object" }
   )
   return completion.choices[0].message.content
 
 def get_real_prompt(prompt,task_desc):
-  return prompt.replace('<user_command>',task_desc)
+  real_prompt=prompt.replace('<user_command>',task_desc)
+  return real_prompt
+def get_real_response(response):
+  return response.replace("<task>", "").replace("<t_end>", "")
+
+def get_task_desc_from_csv(file_path):
+  task_desc=[]
+  with open(file_path, 'r',encoding='utf-8') as file:
+    reader = csv.reader(file)
+    for row in reader:
+      task_desc.append( row[0])
+  return task_desc
+
+def write_to_csv(file_path, string_array):
+  data = [[string] for string in string_array]
+  with open(file_path, 'w',encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
